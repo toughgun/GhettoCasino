e1430cfe9cc5cffe6431c7e6a9081a878a175531
akoli-pilot <163882224+akoli-pilot@users.noreply.github.com>
2025-03-01 11:52:40 -0800

    add calculate_framerate()

--- akoli.cpp
+++ akoli.cpp
@@ -1,6 +1,18 @@
 // Phillip Lakes (Koli)
 // CS 3350 Software Engineering
 //
+#include <chrono>
+
+std::chrono::time_point<std::chrono::high_resolution_clock> last_time = std::chrono::high_resolution_clock::now();
+
+void calculate_framerate() {
+    auto current_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> elapsed = current_time - last_time;
+    last_time = current_time;
+    float fps = 1.0f / elapsed.count();
+    cout << "FPS: " << fps << endl;
+}
+
 int main(int argc char *argv[] ) {
 
   return 0;
