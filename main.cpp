2cc96736526474bab03045100cea779f0129e476
akoli-pilot <163882224+akoli-pilot@users.noreply.github.com>
2025-03-07 21:22:08 -0800

    Create main.cpp
    Former-commit-id: f59e836763c25f5991eb2bfbc546aa7a9d2a90ca

---
+++ main.cpp
@@ -0,0 +1,12 @@
+int main(int argc, char *argv[]) {
+    /*
+    for(int i=0;i<3;++i) {
+        reels[x] = new Reel();
+    }
+    */
+    //int done = 0;
+    //int winactive = 0;
+    //initGL();
+
+    return 0;
+}
