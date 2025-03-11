c11956ba71fa5514c4a9c17b0d77885fe4363f09
haonanc00 <haonanc123@gmail.com>
2025-03-11 09:43:04 -0700

    Update hchen.cpp

--- hchen.cpp
+++ hchen.cpp
@@ -4,3 +4,149 @@
 // {
 //     ggprint8b(r, 0x00ff00ff, "Haonan");
 // }
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>  // Needed for cos() and sin()
+#include <GL/gl.h>
+#include <GL/glx.h>
+//#include "hchen_functions.h"
+#include <cstring>
+
+/*
+class Texture {
+public:
+	//Image *backImage;
+	GLuint backTexture;
+	float xc[2];
+	float yc[2];
+};
+
+class Global {
+public:
+	int xres, yres;
+	Texture tex;
+	Global() {
+		xres=640, yres=480;
+	}
+} g;
+
+class X11_wrapper {
+private:
+	Display *dpy;
+	Window win;
+	GLXContext glc;
+public:
+	X11_wrapper() {
+		GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+		//GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, None };
+		setup_screen_res(640, 480);
+		dpy = XOpenDisplay(NULL);
+		if(dpy == NULL) {
+			printf("\n\tcannot connect to X server\n\n");
+			exit(EXIT_FAILURE);
+		}
+		Window root = DefaultRootWindow(dpy);
+		XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
+		if(vi == NULL) {
+			printf("\n\tno appropriate visual found\n\n");
+			exit(EXIT_FAILURE);
+		} 
+		Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
+		XSetWindowAttributes swa;
+		swa.colormap = cmap;
+		swa.event_mask =
+			ExposureMask | KeyPressMask | KeyReleaseMask | PointerMotionMask |
+			ButtonPressMask | ButtonReleaseMask |
+			StructureNotifyMask | SubstructureNotifyMask;
+		win = XCreateWindow(dpy, root, 0, 0, g.xres, g.yres, 0,
+								vi->depth, InputOutput, vi->visual,
+								CWColormap | CWEventMask, &swa);
+		set_title();
+		glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
+		glXMakeCurrent(dpy, win, glc);
+	}
+	void cleanupXWindows() {
+		XDestroyWindow(dpy, win);
+		XCloseDisplay(dpy);
+	}
+	void setup_screen_res(const int w, const int h) {
+		g.xres = w;
+		g.yres = h;
+	}
+	void reshape_window(int width, int height) {
+		//window has been resized.
+		setup_screen_res(width, height);
+		glViewport(0, 0, (GLint)width, (GLint)height);
+		glMatrixMode(GL_PROJECTION); glLoadIdentity();
+		glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+		glOrtho(0, g.xres, 0, g.yres, -1, 1);
+		set_title();
+	}
+	void set_title() {
+		//Set the window title bar.
+		XMapWindow(dpy, win);
+		XStoreName(dpy, win, "scrolling background (seamless)");
+	}
+	bool getXPending() {
+		return XPending(dpy);
+	}
+	XEvent getXNextEvent() {
+		XEvent e;
+		XNextEvent(dpy, &e);
+		return e;
+	}
+	void swapBuffers() {
+		glXSwapBuffers(dpy, win);
+	}
+	void check_resize(XEvent *e) {
+		//The ConfigureNotify is sent by the
+		//server if the window is resized.
+		if (e->type != ConfigureNotify)
+			return;
+		XConfigureEvent xce = e->xconfigure;
+		if (xce.width != g.xres || xce.height != g.yres) {
+			//Window size did change.
+			reshape_window(xce.width, xce.height);
+		}
+	}
+} x11;
+*/
+/*
+int show_gameinfo = 0;
+
+void gameInfo_hc()//int x, int y)
+{
+
+    drawString(20, 50, "gameinfo");
+    *
+    Rect r;
+    r.bot = y;
+    r.left = x;
+    r.center = 0;
+    ggprint8b(&r, 8, 0x00ff00ff, "GameInfo");
+/
+}
+*/
+ void drawString(int left, int top, char *str)
+{
+//    XDrawString(100, 200, g.glc, left, top, "gameinfo", 8);
+
+}
+void gameInfo()
+{
+}
+
+
+/*void show_hchen(Rect *r)
+{
+    ggprint8b(r, 0x00ff00ff, "Haonan");
+}*/
