fc7c2ee6d3617e3ec06ef6a18aabe4580929f3ed
haonanc00 <haonanc123@gmail.com>
2025-03-11 09:44:07 -0700

    Create hchen_functions.h
    Former-commit-id: e145c80a4eca551aaffee1aed73b089470eb3bac

---
+++ hchen_functions.h
@@ -0,0 +1,10 @@
+#include "hchen.cpp"
+#include "fonts.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
