4c34b7365a8ed7e9cae7949acbc74358d6def1a4
haonanc00 <haonanc123@gmail.com>
2025-03-31 15:23:51 -0700

    Update hchen.cpp
    Former-commit-id: 80f0ff3c88ea2d294697ff8721553d5178705354

--- hchen.cpp
+++ hchen.cpp
@@ -1,5 +1,9 @@
-// #include "fonts.h"
+//Haonan Chen
+//March 31, 2025
+//
+//has intro animation along with black jack hands
 
+// #include "fonts.h"
 // void show_hchen(Rect *r)
 // {
 //     ggprint8b(r, 0x00ff00ff, "Haonan");
@@ -14,79 +18,91 @@
 #include "hchen_functions.h"
 #include "global.h"
 #include "image.h"
+#include "bolayvar.h"
+#include "blackjack.h"
 #include <time.h>
 void game_info()
 {
     /*
-    Rect r;
-        r.bot = 0;
-        r.left = 0;
-        ggprint8b(&r, 16, 0xffffff, "GHETTO CASINO");
-        glPopMatrix();
-        */
+       Rect r;
+       r.bot = 0;
+       r.left = 0;
+       ggprint8b(&r, 16, 0xffffff, "GHETTO CASINO");
+       glPopMatrix();
+       */
 }
 
 void intro_physics()
 {
     //set image as logo
     //make logo physics as scrolling down
-        g.tex.yc[0] -= 0.1;
-        g.tex.yc[1] -= 0.1;
+    g.tex.yc[0] -= 0.1;
+    g.tex.yc[1] -= 0.1;
 }
 
 //set start time for when intro starts
 //set intro playing as true
-time_t start_time = time(NULL);
+time_t start_time;
 time_t current_time;
 bool introplay = true;
 void intro_render()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    //glClear(GL_COLOR_BUFFER_BIT);
     //get current time to check if its over 5 seconds
     //if not render game logo and call for intro_physics()
+    start_time = time(NULL);
     current_time = time(NULL);
-    while (current_time - start_time >= 5) {
-        intro_physics();
-        /*
-    glPushMatrix();
-    glEnable(GL_ALPHA_TEST);
-    glAlphaFunc(GL_GREATER, 0.0f);
-    glBindTexture(GL_TEXTURE_2D, g.tex.menulogotex);
-    glColor4f(1.0f, 1.0f, 1.0f, 0.8f);
-    glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 0.0f);
-        glVertex2f(0,0);
-        //glVertex2f(-g.tex.menuLogo/2, -g.tex.menuLogo/2);
-
-        glTexCoord2f(1.0f, 0.0f);
-        glVertex2f(0,g.yres);
-        //glVertex2f( g.tex.menuLogo/2, -g.tex.menuLogo/2);
-
-        glTexCoord2f(1.0f, 1.0f);
-        glVertex2f(g.xres, g.yres);
-        //glVertex2f( g.tex.menuLogo/2,  g.tex.menuLogo/2);
-
-        glTexCoord2f(0.0f, 1.0f);
-        glVertex2f(g.xres, 0);
-        //glVertex2f(-g.tex.menuLogo/2,  g.tex.menuLogo/2);
-    glEnd();
-    glDisable(GL_ALPHA_TEST);
-    glPopMatrix();
-    */
+    while (current_time - start_time >= 150) {
+        current_time = time(NULL);
+        //intro_physics();
+        drawMenuLogo();
     }
-    //set intro playing to false
     introplay = false;
+    //glClear(GL_COLOR_BUFFER_BIT);
 }
 
- void drawString(int left, int top, char *str)
+void dealerHands()
 {
-//    XDrawString(100, 200, g.glc, left, top, "gameinfo", 8);
+    //checks if ACE was card
+    if (bj.shoe[1] != 1) {
+        //if not ace, checks if jack, queen, king
+        if (bj.shoe[1] == 11 || bj.shoe[1] == 12 || bj.shoe[1] == 13) {
+            bj.dealerHand[bj.shoe[1]] =+ bj.shoe[1];
+            bj.dealerHandTotal =+ 10;
+        } else {
+            bj.dealerHand[bj.shoe[1]] =+ bj.shoe[1];
+            bj.dealerHandTotal =+ bj.shoe[1];
+        }
+    } else {
+        if (bj.dealerHandTotal + bj.shoe[1] <= 21)
+            bj.dealerHandTotal =+ bj.shoe[2];
+        else
+            bj.dealerHandTotal =+ 1;
+    }
 
 }
-
-
+void playerHands()
+{
+    //checks if ACE was card
+    if (bj.shoe[2] != 1) {
+        //if not ace, checks for jack, queen, king
+        if (bj.shoe[2] == 11 || bj.shoe[2] == 12 || bj.shoe[2] == 13) {
+            bj.playerHand[bj.shoe[2]] =+ bj.shoe[2];
+            bj.dealerHandTotal =+ 10;
+        } else {
+            bj.playerHand[bj.shoe[2]] =+ bj.shoe[2];
+            bj.playerHandTotal =+ bj.shoe[2];
+        }
+    } else {
+        if (bj.playerHandTotal + bj.shoe[2] <= 21)
+            bj.playerHandTotal =+  11;
+        else
+            bj.playerHandTotal =+ 1;
+    }
+}
 /*void show_hchen(Rect *r)
-{
+  {
 
-    ggprint8b(r, 0x00ff00ff, "Haonan");
-}*/
+  ggprint8b(r, 0x00ff00ff, "Haonan");
+  }*/
+
