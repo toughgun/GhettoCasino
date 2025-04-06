e6cc28bab43bf6ab7dfa42d2819395bcae46d3e9
haonanc00 <haonanc123@gmail.com>
2025-04-06 13:56:43 -0700

    intro animation works
    Former-commit-id: f4bc4b6ef3a21fbb5e1a05aae5581aea5d3bfeec

--- hchen.cpp
+++ hchen.cpp
@@ -1,9 +1,8 @@
 //Haonan Chen
-//March 31, 2025
+//april 6, 2025
 //
 //has intro animation along with black jack hands
 
-// #include "fonts.h"
 // void show_hchen(Rect *r)
 // {
 //     ggprint8b(r, 0x00ff00ff, "Haonan");
@@ -20,89 +19,142 @@
 #include "image.h"
 #include "bolayvar.h"
 #include "blackjack.h"
-#include <time.h>
-void game_info()
-{
-    /*
-       Rect r;
-       r.bot = 0;
-       r.left = 0;
-       ggprint8b(&r, 16, 0xffffff, "GHETTO CASINO");
-       glPopMatrix();
-       */
-}
+#include "fonts.h"
+#include <ctime>
 
+time_t current_time = 0;
+time_t start_time = 0;
+int introplay = 1;
+bool introstart = false;
+float centerX;
+float centerY;
+//bool logoset = false;
+//float logoPosy;
+void game_info(Rect *r)
+{
+    // Rect r;
+    // r.bot = 0;
+    // r.left = 0;
+    // ggprint8b(&r, 16, 0xffffff, "GHETTO CASINO");
+    // glPopMatrix();
+    ggprint8b(r, 16, 0x00ff00ff, "WELCOME");
+}
+void init_intro_logo()
+{
+    g.tex.logo_xc[0] = 0.0f;  // Left of the texture
+    g.tex.logo_xc[1] = 1.0f;  // Right of the texture
+    g.tex.logo_yc[0] = 0.0f;  // Start at the top of the texture
+    g.tex.logo_yc[1] = 1.0f;  // Start at the bottom of the texture
+
+}
 void intro_physics()
 {
     //set image as logo
     //make logo physics as scrolling down
-    g.tex.yc[0] -= 0.1;
-    g.tex.yc[1] -= 0.1;
-}
+    float elapsedTime = time(nullptr) - start_time;
+    // Time passed since intro started
+    //if (elapsedTime < 4.9) {
+    float maxSpeed = 0.5f;      // Maximum speed at the start
+    float damping = 0.1f;       // How fast it slows down over time
+    float currentSpeed = maxSpeed / (1.0f + damping * elapsedTime);
+    //logoPosy -= currentSpeed;
+    //else if (!logoset){
+      //  logoPosy = g.yres /2.0f;
+       // logoset = true;
 
-//set start time for when intro starts
-//set intro playing as true
-time_t start_time;
-time_t current_time;
-bool introplay = true;
+    g.tex.logo_yc[0] -= currentSpeed;
+    g.tex.logo_yc[1] -= currentSpeed;
+    if (g.tex.logo_yc[0] < 0.0f) {
+        g.tex.logo_yc[0] = 1.0f;
+        g.tex.logo_yc[1] = 0.0f;
+    }
+}
+void intro_logo()
+{
+    centerX = g.xres / 2.0f;
+    centerY = g.yres / 2.0f;
+    glColor4f(1.0f, 1.0f, 1.0f, 0.8f);
+    glPushMatrix();
+    glTranslatef(centerX, centerY, 0.0f);
+    glBindTexture(GL_TEXTURE_2D, g.tex.menulogotex);
+            float h = g.tex.menuLogo->height/2.0f;
+            float w = g.tex.menuLogo->width/2.0f;
+            glBegin(GL_QUADS);
+                // Bottom-left
+                glTexCoord2f(g.tex.logo_xc[0], g.tex.logo_yc[1]);
+                glVertex2f(-w,  h);
+                // Bottom-right
+                glTexCoord2f(g.tex.logo_xc[1], g.tex.logo_yc[1]);
+                glVertex2f(w,  h);
+                // Top-right
+                glTexCoord2f(g.tex.logo_xc[1], g.tex.logo_yc[0]);
+                glVertex2f(w, -h);
+                // Top-left
+                glTexCoord2f(g.tex.logo_xc[0], g.tex.logo_yc[0]);
+                glVertex2f(-w, -h);
+            glEnd();
+            glPopMatrix();
+}
 void intro_render()
 {
-    //glClear(GL_COLOR_BUFFER_BIT);
-    //get current time to check if its over 5 seconds
-    //if not render game logo and call for intro_physics()
-    start_time = time(NULL);
-    current_time = time(NULL);
-    while (current_time - start_time >= 150) {
-        current_time = time(NULL);
-        //intro_physics();
-        drawMenuLogo();
+    if (introplay == 1) {
+        if (!introstart) {
+            init_intro_logo();
+            start_time = time(nullptr);
+            //logoPosy = g.yres + 100.0f;
+            introstart = true;
+            //logoset = false;
+        }
+
+        current_time = time(nullptr);
+        if (current_time - start_time < 5) {
+
+            intro_logo();
+            //drawMenuLogo();
+            intro_physics();
+        } else {
+            introstart = false;
+            introplay = 0;
+        }
     }
-    introplay = false;
-    //glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void dealerHands()
+void dealerHands(int x)
 {
     //checks if ACE was card
-    if (bj.shoe[1] != 1) {
+    if (bj.shoe[x] != 1) {
         //if not ace, checks if jack, queen, king
-        if (bj.shoe[1] == 11 || bj.shoe[1] == 12 || bj.shoe[1] == 13) {
-            bj.dealerHand[bj.shoe[1]] =+ bj.shoe[1];
+        if (bj.shoe[x] == 11 || bj.shoe[x] == 12 || bj.shoe[x] == 13) {
+            bj.dealerHand[bj.shoe[x]] =+ bj.shoe[x];
             bj.dealerHandTotal =+ 10;
         } else {
-            bj.dealerHand[bj.shoe[1]] =+ bj.shoe[1];
-            bj.dealerHandTotal =+ bj.shoe[1];
+            bj.dealerHand[bj.shoe[x]] =+ bj.shoe[x];
+            bj.dealerHandTotal =+ bj.shoe[x];
         }
     } else {
-        if (bj.dealerHandTotal + bj.shoe[1] <= 21)
-            bj.dealerHandTotal =+ bj.shoe[2];
+        if (bj.dealerHandTotal + bj.shoe[x] <= 21)
+            bj.dealerHandTotal =+ bj.shoe[x];
         else
             bj.dealerHandTotal =+ 1;
     }
 
 }
-void playerHands()
+void playerHands(int x)
 {
     //checks if ACE was card
-    if (bj.shoe[2] != 1) {
+    if (bj.shoe[x] != 1) {
         //if not ace, checks for jack, queen, king
-        if (bj.shoe[2] == 11 || bj.shoe[2] == 12 || bj.shoe[2] == 13) {
-            bj.playerHand[bj.shoe[2]] =+ bj.shoe[2];
+        if (bj.shoe[x] == 11 || bj.shoe[x] == 12 || bj.shoe[x] == 13) {
+            bj.playerHand[bj.shoe[x]] =+ bj.shoe[x];
             bj.dealerHandTotal =+ 10;
         } else {
-            bj.playerHand[bj.shoe[2]] =+ bj.shoe[2];
-            bj.playerHandTotal =+ bj.shoe[2];
+            bj.playerHand[bj.shoe[x]] =+ bj.shoe[x];
+            bj.playerHandTotal =+ bj.shoe[x];
         }
     } else {
-        if (bj.playerHandTotal + bj.shoe[2] <= 21)
+        if (bj.playerHandTotal + bj.shoe[x] <= 21)
             bj.playerHandTotal =+  11;
         else
             bj.playerHandTotal =+ 1;
     }
 }
-/*void show_hchen(Rect *r)
-  {
-
-  ggprint8b(r, 0x00ff00ff, "Haonan");
-  }*/
-
