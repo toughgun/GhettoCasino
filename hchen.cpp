65215e00a0b01e948e59dec63241efd60f1595d1
haonanc00 <haonanc123@gmail.com>
2025-04-15 16:38:50 -0700

    change intro a bit
    Former-commit-id: f44d3a4ae831449a18435f5e8ce7be44addcfd58

--- hchen.cpp
+++ hchen.cpp
@@ -1,12 +1,10 @@
 //Haonan Chen
 //april 6, 2025
 //
-//has intro animation along with black jack hands
+//This file has
+//intro animation
+//black jack player/dealer hands logic
 
-// void show_hchen(Rect *r)
-// {
-//     ggprint8b(r, 0x00ff00ff, "Haonan");
-// }
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -28,8 +26,7 @@ int introplay = 1;
 bool introstart = false;
 float centerX;
 float centerY;
-//bool logoset = false;
-//float logoPosy;
+float logoPosY = 0.0f;
 void game_info(Rect *r)
 {
     // Rect r;
@@ -45,72 +42,62 @@ void init_intro_logo()
     g.tex.logo_xc[1] = 1.0f;  // Right of the texture
     g.tex.logo_yc[0] = 0.0f;  // Start at the top of the texture
     g.tex.logo_yc[1] = 1.0f;  // Start at the bottom of the texture
-
+    logoPosY = g.yres + 150.0f;
 }
 void intro_physics()
 {
-    //set image as logo
-    //make logo physics as scrolling down
     float elapsedTime = time(nullptr) - start_time;
-    // Time passed since intro started
-    //if (elapsedTime < 4.9) {
-    float maxSpeed = 0.3f;      // Maximum speed at the start
-    float damping = 0.1f;       // How fast it slows down over time
-    float currentSpeed = maxSpeed / (1.0f + damping * elapsedTime);
-    //logoPosy -= currentSpeed;
-    //else if (!logoset){
-      //  logoPosy = g.yres /2.0f;
-       // logoset = true;
+    float startY = g.yres + 150.0f;
+    float endY = g.yres / 2.0f;
+    float duration = 5.0f;
 
-    g.tex.logo_yc[0] -= currentSpeed;
-    g.tex.logo_yc[1] -= currentSpeed;
-    if (g.tex.logo_yc[0] < 0.0f) {
-        g.tex.logo_yc[0] = 1.0f;
-        g.tex.logo_yc[1] = 0.0f;
-    }
+    float t = elapsedTime / duration;
+
+    if (elapsedTime >= 5.0f)
+        return;
+
+    if (t > 1.0f)
+        t = 1.0f;
+
+    logoPosY = startY * (1.0f - t) + endY * t;
 }
 void intro_logo()
 {
-    centerX = g.xres / 2.0f;
-    centerY = g.yres / 2.0f;
+    float centerX = g.xres / 2.0f;
+    float centerY = logoPosY;
+
     glColor4f(1.0f, 1.0f, 1.0f, 0.8f);
     glPushMatrix();
     glTranslatef(centerX, centerY, 0.0f);
+
     glBindTexture(GL_TEXTURE_2D, g.tex.menulogotex);
-            float h = g.tex.menuLogo->height/2.0f;
-            float w = g.tex.menuLogo->width/2.0f;
-            glBegin(GL_QUADS);
-                // Bottom-left
-                glTexCoord2f(g.tex.logo_xc[0], g.tex.logo_yc[1]);
-                glVertex2f(-w,  h);
-                // Bottom-right
-                glTexCoord2f(g.tex.logo_xc[1], g.tex.logo_yc[1]);
-                glVertex2f(w,  h);
-                // Top-right
-                glTexCoord2f(g.tex.logo_xc[1], g.tex.logo_yc[0]);
-                glVertex2f(w, -h);
-                // Top-left
-                glTexCoord2f(g.tex.logo_xc[0], g.tex.logo_yc[0]);
-                glVertex2f(-w, -h);
-            glEnd();
-            glPopMatrix();
+
+    float h = g.tex.menuLogo->height / 2.0f;
+    float w = g.tex.menuLogo->width / 2.0f;
+
+    glBegin(GL_QUADS);
+        glTexCoord2f(g.tex.logo_xc[0], g.tex.logo_yc[0]); glVertex2f(-w,  h);
+        glTexCoord2f(g.tex.logo_xc[1], g.tex.logo_yc[0]); glVertex2f( w,  h);
+        glTexCoord2f(g.tex.logo_xc[1], g.tex.logo_yc[1]); glVertex2f( w, -h);
+        glTexCoord2f(g.tex.logo_xc[0], g.tex.logo_yc[1]); glVertex2f(-w, -h);
+    glEnd();
+    glPopMatrix();
 }
 void intro_render()
 {
+     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     if (introplay == 1) {
         if (!introstart) {
             init_intro_logo();
             start_time = time(nullptr);
             //logoPosy = g.yres + 100.0f;
             introstart = true;
-            //logoset = false;
         }
 
         current_time = time(nullptr);
-        if (current_time - start_time < 5) {
-
+        if (current_time - start_time < 6) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             intro_logo();
-            //drawMenuLogo();
             intro_physics();
         } else {
             introstart = false;
