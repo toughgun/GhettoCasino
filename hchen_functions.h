f8297d59407f47a3364584f69a3f3e4187fa4119
haonanc00 <haonanc123@gmail.com>
2025-04-22 09:45:58 -0700

    Update hchen_functions.h
    Former-commit-id: 9c3ba278cf9f4c45d0db627b0858f4d0d639d2f8

--- hchen_functions.h
+++ hchen_functions.h
@@ -4,14 +4,22 @@
 extern void intro_physics();
 extern void game_info();
 extern void intro_render();
+extern int introplay;
+extern bool introstart;
+
 extern void dealerHands();
 extern void playerHands();
+extern void bjPayout();
+extern void bjButtonRender();
+extern void renderDoubleButton();
+extern void renderSplitButton();
+extern void renderInsuranceButton();
+extern int splitVal;
+
 extern void gameInfo();
 extern void displayInfo();
 extern void infoRetangle();
-extern int introplay;
 extern bool infoButton(int x, int y);
 extern bool disInfo;
-extern bool introstart;
 
 #endif
