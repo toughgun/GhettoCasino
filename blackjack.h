16fb1a7274675697add196650a8bdc4e8ad532dc
haonanc00 <haonanc123@gmail.com>
2025-04-22 08:17:41 -0700

    Update blackjack.h
    Former-commit-id: b146eb731edd100b06c14d167459ebec28bd8655

--- blackjack.h
+++ blackjack.h
@@ -14,6 +14,7 @@ class Blackjack {
     bool shuffled;
     bool delt;
     bool split;
+    bool insure;
     // GLuint cards;
     float cardWidth;
     float cardHeight;
@@ -29,6 +30,7 @@ class Blackjack {
         shuffled        = false;
         delt            = false;
         split           = false;
+        insure          = false;
     }
 };
 
