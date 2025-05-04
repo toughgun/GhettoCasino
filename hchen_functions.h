edf744a782613ff441de6fd3b2f25458e157acb7
haonanc00 <haonanc123@gmail.com>
2025-05-04 15:52:32 -0700

    Update hchen_functions.h
    Former-commit-id: d250ac27970db5a11cd22250c45e8437eccf3088

--- hchen_functions.h
+++ hchen_functions.h
@@ -1,25 +1,36 @@
 #ifndef HCHEN_H
 #define HCHEN_H
 
-extern void intro_physics();
-extern void game_info();
-extern void intro_render();
-extern int introplay;
-extern bool introstart;
-
-extern void dealerHands(int x);
-extern void playerHands(int x);
-extern void dealerHandrecheck();
-extern void playerHandrecheck();
-extern void bjPayout();
-extern void bjButtonRender();
-extern void renderDoubleButton();
-extern void renderInsuranceButton();
-
 extern void gameInfo(int xx);
 extern void displayInfo();
 extern void infoRetangle();
 extern bool infoButton(int x, int y);
 extern bool disInfo;
 
+extern void intro_physics();
+extern void intro_render();
+extern int introplay;
+extern bool introstart;
+extern void intro_text();
+
+extern void playBJ();
+extern void hit();
+extern void dealerHands(int x);
+extern void playerHands(int x);
+extern void dealerCheckHand();
+extern void dealerHandrecheck();
+extern void playerHandrecheck();
+extern void bjPayout();
+extern bool standRender;
+extern bool hitRender;
+
+extern bool renderBJInfo;
+extern void bjButtonRender();
+extern void bjButtonClick(int x, int y);
+extern void renderDoubleButton();
+extern void renderHitButton();
+extern void renderStandButton();
+extern void bjInfoRender();
+extern void bjInfo();
+extern void bjInfoButton();
 #endif
